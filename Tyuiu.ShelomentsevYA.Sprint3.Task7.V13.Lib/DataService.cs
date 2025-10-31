c3909a1e2d20110d5486dd61baecd461541d3bdd
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-10-31 20:35:15 +0500

    Task7

--- Tyuiu.ShelomentsevYA.Sprint3.Task7.V13.Lib/DataService.cs
+++ Tyuiu.ShelomentsevYA.Sprint3.Task7.V13.Lib/DataService.cs
@@ -5,46 +5,26 @@ namespace Tyuiu.ShelomentsevYA.Sprint3.Task7.V13.Lib
 {
     public class DataService : ISprint3Task7V13
     {
+        
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            
             int len = stopValue - startValue + 1;
-
-            
             double[] result = new double[len];
 
-            
-            int index = 0;
-
-            
+            int i = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                
-                double denominator = Math.Cos(Math.Pow(x, 3)) + 1;
+                double denom = Math.Cos(x) + 1;      
+                double fx;
 
-                if (denominator == 0)
-                {
-                    
-                    result[index] = 0;
-                }
+                if (denom == 0)
+                    fx = 0;                          
                 else
-                {
-                    
-                    
-                    double fx = 3 * x + 2 - ((2 * x) - x) / denominator;
+                    fx = 3 * x + 2 - (x / denom);
 
-                    
-                    fx = Math.Round(fx, 2);
-
-                    
-                    result[index] = fx;
-                }
-
-                
-                index++;
+                result[i++] = Math.Round(fx, 2);     
             }
 
-            
             return result;
         }
     }
