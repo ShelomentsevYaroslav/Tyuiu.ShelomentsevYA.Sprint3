ad4ffbc97439986cebe8b8dcfdbbb77fe033ed94
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-10-31 20:01:06 +0500

    Task6

---
+++ Tyuiu.ShelomentsevYA.Sprint3.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ShelomentsevYA.Sprint3.Task6.V12.Lib;
+using System;
+
+namespace Tyuiu.ShelomentsevYA.Sprint3.Task6.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            
+            int result = ds.GetSumTheDivisors(9, 18);
+
+            int expected = 221;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.ShelomentsevYA.Sprint3.Task6.V12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tyuiu.ShelomentsevYA.Sprint3.Task6.V12.Lib;
+
+namespace Tyuiu.ShelomentsevYA.Sprint3.Task6.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Обработка целочисленной информации | Вариант 12";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Шеломенцев Я.А. | ИСТНб-25-1                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти среди целых чисел из диапазона [9, 18] сумму всех их делителей.   *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int startValue = 9;
+            int stopValue = 18;
+
+            int result = ds.GetSumTheDivisors(startValue, stopValue);
+
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
